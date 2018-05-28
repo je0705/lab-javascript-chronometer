f13bfef53a6e1c25f6b3a46c32b29595bd9155e6
JensErhardt <jens-erhardt@web.de>
2018-05-28 15:31:16 +0200

    BER-Aperna/Jens

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,23 +1,40 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
 
-// }
+  this.currentTime = 0;
+  this.intervalId = 0;
 
-// Chronometer.prototype.startClick = function () {
+}
 
-// };
+Chronometer.prototype.startClick = function () {
+  var that = this;
+  this.intervallId = setInterval(function (){
+    that.currentTime++;
+  }, 1000)
+};
 
-// Chronometer.prototype.setMinutes = function () {
+Chronometer.prototype.setMinutes = function () {
+  var minutesDec = this.currentTime / 60;
+  var minutesRounded = Math.floor(minutesDec);
+  return minutesRounded;
   
-// };
+};
 
-// Chronometer.prototype.setSeconds = function () {
+ Chronometer.prototype.setSeconds = function () {
+  var secondsLeft = this.currentTime % 60;
+  return secondsLeft;
   
-// };
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (value) {
+ 
+  if (value === 0){
+    value = "0" + value;
+  } else if (value === 1){
+    value = "0" + value;
+  }
+  return value;
+};
 
 // Chronometer.prototype.setTime = function () {
 
@@ -38,3 +55,9 @@
 // Chronometer.prototype.splitClick = function () {
 
 // };
+
+var chrono = new Chronometer;
+// chrono.startClick();
+// chrono.setSeconds();
+// chrono.setMinutes();
+// chrono.twoDigitsNumber(); 
